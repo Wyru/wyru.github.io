9d8dd4740e187c065d19b94bc08abfbee922f29e
Will Saymon <wsaymonoficial@gmail.com>
2020-01-01 18:30:34 -0300

    Updates

---
+++ precache-manifest.a120a9eac75c9c5c71ad568f025b1b01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9901dff3e98afc6fd8a0ff40aa17134d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f600de6122c6912059a",
+    "url": "/static/css/main.ab18e129.chunk.css"
+  },
+  {
+    "revision": "6c182df4a32e9ce82882",
+    "url": "/static/js/2.6cbdf00c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6cbdf00c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f600de6122c6912059a",
+    "url": "/static/js/main.5a72c751.chunk.js"
+  },
+  {
+    "revision": "49974024a0475f2798fb",
+    "url": "/static/js/runtime-main.fd0568dc.js"
+  },
+  {
+    "revision": "9ae8b93045308dab85515b2579e3d108",
+    "url": "/static/media/bg.9ae8b930.jpg"
+  }
+]);
